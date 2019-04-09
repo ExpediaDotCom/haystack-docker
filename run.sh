64b395754db261d9dcd387db4113f9a1c25fb959
Ashish Aggarwal <asaggarwal@expedia.com>
2019-04-09 20:07:27 +0530

    add whitelist fields for demo
    Add error and http.status code as whitelist fields

--- run.sh
+++ run.sh
@@ -1,4 +1,18 @@
 #!/usr/bin/env bash
+
+curl -XPOST -H "Content-Type: application/json" -d '{
+        "fields": [{
+            "name": "error",
+            "type": "string",
+            "enabled": true
+        }, {
+            "name": "http-status",
+            "type": "string",
+            "aliases": [ "http.status_code" ],
+            "enabled": true
+        }]
+}' "http://localhost:9200/reload-configs/whitelist-index-fields/1" 2>1 1>/dev/null
+
 COUNT=0
 URL=$1
 
